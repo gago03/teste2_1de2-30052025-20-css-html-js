0e00330ba230779a546eec30d7003baf88041ce2
gago03 <christianvicaroni12@gmail.com>
2025-05-30 19:58:11 -0300

    Add files via upload

---
+++ scripty.js
@@ -0,0 +1,11 @@
+Function (nome)
+    
+var valor = document.getElementById('numero').value;
+var desconto = document.getElementById('des').value;
+var resultado = valor * desconto/100;
+var resultado2 = valor - resultado;
+
+document.getElementById(resultado).textContent = "comprei um produto por R$" ${valor}", Tive um desconto de R$"${resultado}"e o valor final foi R$"${resultado2};
+
+
+
